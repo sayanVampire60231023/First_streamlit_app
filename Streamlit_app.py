20d5ba14183eff3835d83febe76186ef5eff9010
sayanVampire60231023 <69300561+sayanVampire60231023@users.noreply.github.com>
2022-07-22 19:32:31 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title("my parent new healty diner")
+streamlit.title('my parent new healty diner')
+streamlit.header('My breakfast')
+streamlit.text('omega3 and blueberry oatmel')
+streamlit.text('kale,spinach and rocket smootie')
