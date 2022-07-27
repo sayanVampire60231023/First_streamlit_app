ed5c4c40737f0bfd34978021cda894d6c0bb6444
sayanVampire60231023 <69300561+sayanVampire60231023@users.noreply.github.com>
2022-07-27 14:57:51 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,3 +10,6 @@ streamlit.header("fruityvice fruit advice")
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
