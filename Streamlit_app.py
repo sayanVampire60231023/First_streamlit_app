44270d6e2214701f0a769edad5a84acb3185e5f3
sayanVampire60231023 <69300561+sayanVampire60231023@users.noreply.github.com>
2022-07-29 15:32:47 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,7 +10,11 @@ fruit_selected=streamlit.multiselect("pick some fruit:",list(my_fruit_list.index
 fruit_to_show=my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show)
 streamlit.header("fruityvice fruit advice")
-
+def get_fruityvice_data(fruit_choice):
+    streamlit.write('the user entered',fruit_choice)
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 try:
   #fruit_choice=streamlit.text_input("what food would you like information about?",'kiwi')
   fruit_choice=streamlit.text_input("what food would you like information about?")
@@ -19,11 +23,11 @@ try:
     
   else:
     
-  
-    streamlit.write('the user entered',fruit_choice)
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(get_fruityvice_data(fruit_choice))
+    #streamlit.write('the user entered',fruit_choice)
+    #fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+    #streamlit.dataframe(fruityvice_normalized)
 #fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 except URLError as e:
   streamlit.error()
